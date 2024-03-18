ef82ac72e6eb5eaca30f56a4b8ba222d01ba2ddf
Kellen Rouff <KJR62@pitt.edu>
2024-03-18 16:39:41 -0500

    Update FullRestore.sh

--- FullRestore.sh
+++ FullRestore.sh
@@ -4,7 +4,7 @@
 backup_folder="/home/student/Capstone/Backup"
 
 # Define the destination folder path
-destination_folder="/home/student/Capstone/Test"
+destination_folder="/home/student/Capstone"
 
 # Copy all files from the backup folder to the destination folder
 cp -r "$backup_folder"/* "$destination_folder"
