f9eed26efffcbed8aa70803806e3e95998722961
Kellen Rouff <KJR62@pitt.edu>
2024-03-19 14:49:03 -0500

    Update FullRestore.sh

--- FullRestore.sh
+++ FullRestore.sh
@@ -4,9 +4,26 @@
 backup_folder="/home/student/Capstone/Backup"
 
 # Define the destination folder path
-destination_folder="/home/student/Capstone"
+destination_folder="/home/student/Capstone/Test"
 
 # Copy all files from the backup folder to the destination folder
 cp -r "$backup_folder"/* "$destination_folder"
 
 echo "Files restored successfully."
+
+# MySQL database name
+database="hospital"
+
+# Check if the database exists
+if mysqlshow --defaults-file=/home/student/Capstone/.my.cnf "$database" >/dev/null; then
+    echo "Database already exists."
+else
+    echo "Database does not exist. Creating..."
+    mysql --defaults-file=/home/student/Capstone/.my.cnf -e "CREATE DATABASE $database;"
+    echo "Database created."
+fi
+
+# Restore the MySQL database from the backup
+mysql --defaults-file=/home/student/Capstone/.my.cnf "$database" < "$backup_folder"/hospital_backup.sql
+
+echo "Database restored successfully."
